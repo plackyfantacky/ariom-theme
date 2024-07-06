043b066bb9520cb471f6a7d511ee5aae2005f27a
Adam Trickett <adamt@workhouse.au>
2024-07-07 01:11:32 +0800

    more style updates. added experience timeline pattern (TODO currently out of sync with WP)

--- functions.php
+++ functions.php
@@ -3,4 +3,6 @@
     add_action('wp_enqueue_scripts', 'ariom_enqueue_styles');
     function ariom_enqueue_styles() {
         wp_enqueue_style('ariom-style', get_stylesheet_directory_uri() . '/css/ariom.css', [], filemtime(get_stylesheet_directory() . '/css/ariom.css'));
+
+        wp_enqueue_script('ariom-script', get_stylesheet_directory_uri() . '/js/frontend.js', [], filemtime(get_stylesheet_directory() . '/js/frontend.js'), true);
     }
