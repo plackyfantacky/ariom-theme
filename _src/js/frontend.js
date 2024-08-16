261d1a6d00fb591f3fe5b14c8e0493185b261cd5
Adam Trickett <adamt@workhouse.au>
2024-08-16 17:43:57 +0800

    more theme styling updates. includes link smooth scrolling

--- _src/js/frontend.js
+++ _src/js/frontend.js
@@ -176,3 +176,54 @@ window.addEventListener('resize', function() {
         drawBranchTimeline(educCtx, educCanvas, educContainer, educItems);
     }
 });
+
+//dom content loaded
+window.addEventListener('load', function() {
+    /*  find all input/textarea/select tags. if there is an 'aria-required' attribute, add a HTML5 'required' attribute as well */
+    const formElements = document.querySelectorAll('input, textarea, select');
+    formElements.forEach(function(el) {
+        if(el.hasAttribute('aria-required')) {
+            el.setAttribute('required', 'required');
+            //add a span element after to display a tick or cross
+            const span = document.createElement('span');
+            span.classList.add('icon');
+            el.parentNode.insertBefore(span, el.nextSibling);
+        }
+    });
+});
+
+//utility function to check make #links scroll smoothly
+jQuery(document).ready(function ($) {
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1500, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+});
